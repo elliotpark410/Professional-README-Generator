0d09a1720344a3f6a41dbc871372ccbb7847b784
Elliot Park <elliotpark410@gmail.com>
2022-01-24 23:25:08 -0800

    include functions to generate license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,36 @@
-// NEED HELP ADDING LICENSE BADGE!!!!!!!
-
-`[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)]`
-
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge and link based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if (license[0] === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license[0] === 'Apache_2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license[0] === 'GPLv3') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else {
+    return "";
+  }
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license[0] === 'MIT') {
+    return `* [License](#license)`;
+  } else if (license[0] === 'Apache_2.0') {
+    return `* [License](#license)`;
+  } else if (license[0] === 'GPLv3') {
+    return `* [License](#license)`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
 
 
   ## Description
@@ -29,7 +41,7 @@ function generateMarkdown(data) {
   * [Usage Information](#usage)
   * [Contribution Guidelines](#contribution)
   * [Test Instructions](#test)
-  * [License](#license)
+  ${renderLicenseBadge(data.license)}
   * [Questions](#questions)
   
 
@@ -44,6 +56,9 @@ function generateMarkdown(data) {
 
   ## Test Instructions
   ${data.test}
+
+  ## License
+  ${data.license}
 
   ## Questions
   * If you'd like to learn more about this application, check out my [GitHub profile](https://github.com/${data.github})
